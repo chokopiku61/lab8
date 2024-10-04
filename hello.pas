6ddac09283ccaae95573b97260c214bf75fb551c
Тимофей Базолий <tim.23tim23.23@gmail.com>
2024-10-04 13:10:17 +0300

    Теперь программа здаровается в зависимости от часа пользователя

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-  Println('Привет, мир!');
+  var hour := ReadInteger('Введите текущий час:');
+  
+  case hour of
+    4..10: Println('Доброе утро, мир!');
+    11..16: Println('Добрый день, мир!');
+    17..22: Println('Добрый вечер, мир!');
+    else Println('Доброй ночи, мир!');
+  end;
+ 
 end.
