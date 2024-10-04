b4fff66f1f72044f433ed4301af38963b300e875
Тимофей Базолий <tim.23tim23.23@gmail.com>
2024-10-04 13:19:11 +0300

    Добавлен файл time.pas

---
+++ time.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var year := ReadInteger('Введите год:');
+  
+  if (year mod 4 = 0) and not ((year mod 400 <> 0) and (year mod 100 = 0)) then
+    Println('Высокосный')
+  else
+    Println('Не высокосный');
+end.
